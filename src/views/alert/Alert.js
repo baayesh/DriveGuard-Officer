3895f581a5c4cf68f848ce35b9950ddcbabcaccd
Shashika Kulasekara <shashika.t@hotmail.com>
2024-10-28 21:06:13 +0530

    Improvement to Alert View
    Added Display box component.

--- src/views/alert/Alert.js
+++ src/views/alert/Alert.js
@@ -1,12 +1,19 @@
 import './Alert.css';
 import HeaderBox from '../../components/objects/HeaderBox/HeaderBox';
 import TabNavigation from '../../components/navbar/TabNavigation';
+import DisplayBox from '../../components/objects/DisplayBox/DisplayBox';
 const Alert = () => {
+
+    //Display Alerts
+    let displayAlert = "";
+
     return ( 
         <div className="container">
             <HeaderBox headertext={"Alerts"}/>
             <div className='page-content-alert'>
-                
+                <div className='display-alert-wrapper'>
+                    <DisplayBox displayBoxContent={displayAlert}/>
+                </div>
             </div>
             <TabNavigation bgalert={"rgb(10, 55, 202)"}/>
         </div>
