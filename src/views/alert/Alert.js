f7aeb94a79f8bff8c3f94d55d508d560fb11e63a
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-11-15 13:19:54 +0530

    design ui in driveguard officer

--- src/views/alert/Alert.js
+++ src/views/alert/Alert.js
@@ -1,9 +1,8 @@
 import "./Alert.css";
 import HeaderBox from "../../components/objects/HeaderBox/HeaderBox";
 import TabNavigation from "../../components/navbar/TabNavigation";
-import DisplayBox from "../../components/objects/DisplayBox/DisplayBox";
 import { useEffect, useState } from "react";
-import { getFineList,makeFineWitnessed } from "../../middleware/data/dataApis";
+import { getFineList, makeFineWitnessed } from "../../middleware/data/dataApis";
 import { toast, Bounce, ToastContainer } from "react-toastify";
 
 const Alert = () => {
@@ -17,106 +16,113 @@ const Alert = () => {
   useEffect(() => {
     const getWitnessingOffences = async () => {
       const response = await getFineList(officerId);
-
-      if(response !== null && response !== undefined){
-       setIssuedOffencesList(response.data)
-      }
-      else{
+      if (response !== null && response !== undefined) {
+        setIssuedOffencesList(response.data);
+      } else {
         console.error("Error setting array, response is null or undeined");
       }
-    
     };
     getWitnessingOffences();
-    
   }, [handleWitnessRan]);
 
-
-  const handleWitness = async (fineId) =>{
-    const loading = toast.loading("Witnessing...")
-    try{
-    
-        const response = await makeFineWitnessed(fineId);
-        if(response.status === 200){
-            toast.update(loading, {
-                render: "Witness Successful",
-                type: "success",
-                isLoading: false,
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-                transition: Bounce,
-              });
-              if(handleWitnessRan === true ){
-                setHandleWitnessRan(false);
-              }else{
-                setHandleWitnessRan(true);
-              }
-
-        }else{
-            toast.update(loading, {
-                render: "Witness Unsuccessful",
-                type: "error",
-                isLoading: false,
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-                transition: Bounce,
-              });
-
-        }
-
-
-    }catch(err){
-        console.error("handle witness function error: ", err)
+  const handleWitness = async (fineId) => {
+    const loading = toast.loading("Witnessing...");
+    try {
+      const response = await makeFineWitnessed(fineId);
+      if (response.status === 200) {
         toast.update(loading, {
-            render: "Witness Unsuccessful",
-            type: "error",
-            isLoading: false,
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-            transition: Bounce,
-          });
-
+          render: "Witness Successful",
+          type: "success",
+          isLoading: false,
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+        if (handleWitnessRan === true) {
+          setHandleWitnessRan(false);
+        } else {
+          setHandleWitnessRan(true);
+        }
+      } else {
+        toast.update(loading, {
+          render: "Witness Unsuccessful",
+          type: "error",
+          isLoading: false,
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+      }
+    } catch (err) {
+      console.error("handle witness function error: ", err);
+      toast.update(loading, {
+        render: "Witness Unsuccessful",
+        type: "error",
+        isLoading: false,
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
     }
-  }
-
+  };
 
   return (
     <div className="container">
       <HeaderBox headertext={"Alerts"} />
       <div className="page-content-alert">
         <div className="display-alert-wrapper">
-            {issuedOffecensList.map((issuedOffence =>(
-              <div key={issuedOffence.fineId} className="fine-list">
-                <p>{issuedOffence.fineName}</p>
-                <p>{issuedOffence.fineDescription}</p>
-                <p>{issuedOffence.fineAmount}</p>
-                <p>{`${issuedOffence.officerFirstName} ${issuedOffence.officerLastName}`}</p>
-                <p>{issuedOffence.remainingDaysToPay}</p>
-                <button onClick={()=>{handleWitness(issuedOffence.fineId)}}>Be Witness</button>
-
+          {issuedOffecensList.map((issuedOffence) => (
+            <div key={issuedOffence.offenceId} className="fine-list">
+              <div className="offense-fr">
+                <h4>Finned To</h4>
               </div>
-            )))}
-         
+              <div className="offense-sr">
+                <p>{`${issuedOffence.driverFirstName} ${issuedOffence.driverLastName}`}</p>
+                <p>{`${issuedOffence.drivingLicenseNumber}`}</p>
+              </div>
+              <div className="offense-tr">
+                <p className="offense-amt">{`${issuedOffence.offenseAmount}`}</p>
+                <p className="offense-des">{`${issuedOffence.offenseDescription}`}</p>
+              </div>
+              <div className="offense-for">
+                <p className="offense-for-head">Fine Issued By</p>
+                <div className="offense-for-sfr">
+                  <p>{`${issuedOffence.issuedOfficerFirstName} ${issuedOffence.issuedOfficerLastName}`}</p>
+                  <p>{`${issuedOffence.issuedOfficerPoliceId}`}</p>
+                </div>
+           
+              </div>
+
+              <button
+                onClick={() => {
+                  handleWitness(issuedOffence.offenseId);
+                }}
+              >
+                Be Witness
+              </button>
+            </div>
+          ))}
         </div>
       </div>
-      <ToastContainer/>
+      <ToastContainer />
       <TabNavigation bgalert={"rgb(10, 55, 202)"} />
     </div>
   );
--- src/views/settings/Settings.js
+++ src/views/settings/Settings.js
@@ -1,11 +1,19 @@
 import './Settings.css';
 import HeaderBox from '../../components/objects/HeaderBox/HeaderBox';
 import TabNavigation from '../../components/navbar/TabNavigation';
+import { useNavigate } from 'react-router-dom';
 const Settings = () => {
+    const navigate = useNavigate();
+
+    const logout =() => {
+        navigate('/');
+    }
     return ( 
         <div className="container">
             <HeaderBox headertext={"Settings"}/>
             <div className='page-content-settings'>
+
+                <button onClick={logout}>Log Out</button>
                 
             </div>
             <TabNavigation bgsettings={"rgb(10, 55, 202)"}/>
