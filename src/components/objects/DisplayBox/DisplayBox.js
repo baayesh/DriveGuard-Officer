f57d6ff780863b7572bc14fa2fdaff2be5f2ff88
baayesh <ayeshisurukavinda.1998@gmail.com>
2024-11-15 06:30:39 +0530

    add ui changesto search

--- src/components/objects/DisplayBox/DisplayBox.js
+++ src/components/objects/DisplayBox/DisplayBox.js
@@ -1,17 +1,26 @@
-import './DisplayBox.css';
-const DisplayBox = ({firstName, lastName, age, nic, houseNumber, streetName, city}) => {
-    return ( 
+import "./DisplayBox.css";
+const DisplayBox = ({
+  firstName,
+  lastName,
+  age,
+  nic,
+  houseNumber,
+  streetName,
+  city,
+}) => {
+  return (
+    <div >
+      {age !== null && age !== undefined ? (
         <div className="displaybox-content">
-            <div>
-                <h3>{`${firstName} ${lastName}`}</h3>
-                <h4>{age}</h4>
-                <p>{nic}</p>
-                <p>{houseNumber}</p>
-                <p>{streetName}</p>
-                <p>{city}</p>
-            </div>
+          <h3>{`Name: ${firstName} ${lastName}`}</h3>
+          <h4>{`Age: ${age}`}</h4>
+          <p>{`Address: ${houseNumber}, ${streetName}, ${city}`}</p>
         </div>
-     );
-}
- 
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
 export default DisplayBox;
